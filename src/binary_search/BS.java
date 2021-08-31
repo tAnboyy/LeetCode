79cd1aa10452c24c7c833310c6124834db3c5a5b
Thanmay <mdasthanmay@gmail.com>
2021-08-31 21:59:27 +0530

    31-08

---
+++ src/binary_search/BS.java
@@ -0,0 +1,26 @@
+package binary_search;
+//704. Binary Search
+public class BS {
+    public static void main(String[] args) {
+        int[] arr = {-1,0,3,5,9,12};
+        int target = 22;
+        int ans = search(arr, target);
+        System.out.println(ans);
+    }
+
+    public static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end) {
+            int mid = start + ( end - start ) / 2;
+            if(nums[mid] > target) {
+                end = mid - 1;
+            } else if (nums[mid] < target){
+                start = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
